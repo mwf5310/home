9a5c416c69ae0b0e60af98b3a57f40114e9b8508
mwf5310 <mwf5310@163.com>
2018-07-05 07:13:39 +0800

    add folde

---
+++ src/main/suit2/DemoSuite1.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runner.FilterFactoryParams;
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	
+	Test81.class,
+	Test82.class,
+	Test83.class
+})
+public class DemoSuite1 {
+	
+
+}
